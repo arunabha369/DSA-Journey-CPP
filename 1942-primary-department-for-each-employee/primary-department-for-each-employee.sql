6bf0d2f6a836d7924e8f43af0dd37f111c6354df
Arunabha Banerjee <119969705+arunabha369@users.noreply.github.com>
2025-11-18 03:36:49 +0530

    Time: 220 ms (82.09%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,14 +1,8 @@
 SELECT employee_id, department_id
 FROM Employee
-WHERE primary_flag = 'Y'
-
-UNION
-
-SELECT employee_id, department_id
-FROM Employee
-WHERE employee_id IN (
-    SELECT employee_id
-    FROM Employee
+WHERE primary_flag='Y' OR
+    employee_id in
+    (SELECT Employee_id
+    FROM Employee as A
     GROUP BY employee_id
-    HAVING COUNT(*) = 1
-);
+    having count(employee_id)=1);
