3bf5e2e516693beb52422f09db4134cc658169af
Arunabha Banerjee <119969705+arunabha369@users.noreply.github.com>
2025-11-19 19:15:14 +0530

    Time: 65 ms (20.37%) | Memory: 56.8 MB (14.84%) - LeetSync

--- 2741-function-composition/function-composition.js
+++ 2741-function-composition/function-composition.js
@@ -1,12 +1,2 @@
-var compose = function(functions) {
-
-    return function(x) {
-        let result = x;
-
-        for (let i = functions.length - 1; i >= 0; i--) {
-            result = functions[i](result);
-        }
-
-        return result;
-    };
-};
+const compose = functions => x =>
+    functions.reduceRight((acc, fn) => fn(acc), x);
